997c7e09b567aea9242c4f9681d18d7bb5ced61b
Deepanshu Tiwari <tiwari27deepanshu38kt@gmail.com>
2024-11-25 20:54:44 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,4 @@
 
-
 import streamlit as st
 import pandas as pd
 import base64, random
@@ -54,4 +53,6 @@ def show_pdf(file_path):
     with open(file_path,"rb")as f:
         base64_pdf=base64.b64encode(f.read()).decode('utf-8')
     pdf_display=F'<iframe src="data:application/pdf;base64,{base64_pdf}"width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display,unsafe_allow_html=True)
 
+def
