f508cb13d9803e410ef1ae712b9b163332503ace
Deepanshu Tiwari <tiwari27deepanshu38kt@gmail.com>
2024-11-25 21:13:31 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,6 +4,7 @@ import pandas as pd
 import base64, random
 import time,datetime
 
+from nltk.corpus import stopwords
 from mdurl import encode
 from pyresparser import ResumeParser
 from pdfminer3.layout import LAParams, LTTextBox
@@ -12,6 +13,7 @@ from pdfminer3.pdfinterp import PDFResourceManager
 from pdfminer3.pdfinterp import PDFPageInterpreter
 from pdfminer3.converter import TextConverter
 import io,random
+
 
 from streamlit.components.v1 import iframe
 from streamlit_tags import st_tags
@@ -55,4 +57,22 @@ def show_pdf(file_path):
     pdf_display=F'<iframe src="data:application/pdf;base64,{base64_pdf}"width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display,unsafe_allow_html=True)
 
-def
+def course_recommender(course_list):
+    st.subheader("**Courses and Certifications Recommendation 🎓**")
+    c = 0
+    rec_course = []
+    no_of_reco = st.slider('Choose Number of Course Recommendation:',1,10,5)
+    random.shuffle(course_list)
+    for c_name,c_link in course_list:
+        c+=1
+        st.markdown(f"({c}) [{c_name}]({c_link})")
+        rec_course.append(c_name)
+        if c == no_of_reco:
+            break
+    return rec_course
+
+
+# CONNECT TO DATABASE
+
+
+
